d43b0b24ebdcb07cb7e12d4f6c0f1bd54dfb5946
jschold <jscholdny@msn.com>
2015-09-11 11:46:00 -0700

    add findReplace html and form to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,26 @@ var findReplace = function(userString, userWord, newWord) {
     var userStringUp = userString.toUpperCase();
     var userWordUp = userWord.toUpperCase();
     var userWordLow = userWord.toLowerCase();
-    // var newWordUp = newWord.toUpperCase();
     var userReplace = userString.replace(userWord, newWord);
     if (userStringUp.indexOf(userWordUp) >= 0 && newWord) {
         return userReplace;
-    }
-
-    else if (userStringUp.indexOf(userWordUp) >= 0) {
+    } else if (userStringUp.indexOf(userWordUp) >= 0) {
         return userWordLow;
     } else {
         return "That word is not found!"
     }
 };
+
+$(document).ready(function() {
+  $("form#findReplace").submit(function(event) {
+
+    var userString = $("input#userString").val();
+    var userWord = $("input#userWord").val();
+    var newWord = $("input#newWord").val();
+    var result = findReplace(userString, userWord, newWord);
+
+
+    $("#result").text(result);
+    event.preventDefault();
+  });
+});
