fc2498896392b2254520f05eb9a0404a9cb8ec31
jschold <jscholdny@msn.com>
2015-09-11 11:15:28 -0700

    add fifth passing test

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-var findReplace = function(userString, userWord) {
+var findReplace = function(userString, userWord, newWord) {
     var userStringUp = userString.toUpperCase();
     var userWordUp = userWord.toUpperCase();
     var userWordLow = userWord.toLowerCase();
-    if (userStringUp.indexOf(userWordUp) >= 0) {
-    return userWordLow;
-}
+    // var newWordUp = newWord.toUpperCase();
+    var userReplace = userString.replace(userWord, newWord);
+    if (userStringUp.indexOf(userWordUp) >= 0 && newWord) {
+        return userReplace;
+    }
+
+    else if (userStringUp.indexOf(userWordUp) >= 0) {
+        return userWordLow;
+    } else {
+        return "That word is not found!"
+    }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -11,5 +11,7 @@ describe('findReplace', function () {
     it("finds if a word exists in a string with capitalization", function () {
         expect(findReplace("Can I pet your dog?" , "cAn")).to.be.eql("can");
     });
-
+    it("finds if a word exists in a string and replaces it with a different word", function () {
+        expect(findReplace("Can I pet your dog?" , "pet", "walk")).to.be.eql("Can I walk your dog?");
+    });
 });
